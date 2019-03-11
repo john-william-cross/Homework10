5ab2e5fa93a5841c6991efa335ce46a1fda6f1a6
John Cross <jwcross1@dmacc.edu>
2019-03-11 14:48:36 -0500

    added array list type notation to array list and in map

--- src/edu/dmacc/codedsm/homework10/DeckOfCards.java
+++ src/edu/dmacc/codedsm/homework10/DeckOfCards.java
@@ -9,8 +9,8 @@ import java.util.Map;
 public class DeckOfCards {
 
     public static void main(String[] args) {
-        HashMap<String, List> cardDeck = new HashMap<>();               //created hashmap called cardDeck with key of String and a value filled with an integer list
-        ArrayList<Integer> cardValues = new ArrayList();                //created the array list called cardValues that goes in the value of the map
+        HashMap<String, List> cardDeck = new HashMap<String, List>();               //created hashmap called cardDeck with key of String and a value filled with an integer list
+        ArrayList<Integer> cardValues = new ArrayList<Integer>();                //created the array list called cardValues that goes in the value of the map
         String[] suits = {"Hearts", "Clubs", "Diamonds", "Spades"};     //created a string array called suits
         for (String suit : suits) {                                     //for each suit in the suits array (similar to above; "for each Integer i")
             cardDeck.put(suit, cardValues);                             //add the cards to the carddeck; String is suit; List is cardValues
